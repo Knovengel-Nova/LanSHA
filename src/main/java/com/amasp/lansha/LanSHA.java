cbf06c2461e0cf003bf49503f84e0364890a8e60
knovengel <knovengel@knov>
2026-06-19 17:27:55 +0530

    initial commit

---
+++ src/main/java/com/amasp/lansha/LanSHA.java
@@ -0,0 +1,12 @@
+package com.amasp.lansha;
+
+/**
+ *
+ * @author knovengel
+ */
+public class LanSHA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
